80a72d26d4762a0d6ed6b7ee297b01949b9ed471
Daamin909 <ashai.daamin@gmail.com>
2025-03-16 16:57:57 +0530

    connect comms to backend

--- src/Utils/apiClient.js
+++ src/Utils/apiClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: "http://127.0.0.1:5001" });
+const client = axios.create({ baseURL: "https://karen.daamin.hackclub.app" });
 
 export default client;
